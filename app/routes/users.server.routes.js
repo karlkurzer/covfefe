0629d983fd8b197ed8e13eae7fbb9a9e73d60970
Karl Kurzer <kurzer@fzi.de>
2018-02-27 15:06:14 +0100

    removed the oauth strategies and hid the update button for normal users

--- app/routes/users.server.routes.js
+++ app/routes/users.server.routes.js
@@ -1,70 +1,42 @@
 // Invoke 'strict' JavaScript mode
-'use strict';
+"use strict";
 
 // Load the module dependencies
-var users = require('../../app/controllers/users.server.controller'),
-	passport = require('passport');
+var users = require("../../app/controllers/users.server.controller"),
+  passport = require("passport");
 
 // Define the routes module' method
 module.exports = function(app) {
+  app.route("/api/users").get(users.list);
 
-	app.route('/api/users')
-		.get(users.list);
+  // Set up the 'user' parameterized routes
+  app
+    .route("/api/users/:userId")
+    .get(users.read)
+    .put(users.requiresLogin, users.requiresAdmin, users.update)
+    .delete(users.requiresLogin, users.requiresAdmin, users.delete);
 
-	 // Set up the 'user' parameterized routes
-	 app.route('/api/users/:userId')
-		.get(users.read)
-		.put(users.requiresAdmin, users.update)
-		.delete(users.requiresAdmin, users.delete);
- 
-	 // Set up the 'userId' parameter middleware   
-	 app.param('userId', users.userByID);
+  // Set up the 'userId' parameter middleware
+  app.param("userId", users.userByID);
 
-	// Set up the 'signup' routes 
-	app.route('/signup')
-	   .get(users.renderSignup)
-	   .post(users.signup);
+  // Set up the 'signup' routes
+  app
+    .route("/signup")
+    .get(users.renderSignup)
+    .post(users.signup);
 
-	// Set up the 'signin' routes 
-	app.route('/signin')
-	   .get(users.renderSignin)
-	   .post(passport.authenticate('local', {
-			successRedirect: '/',
-			failureRedirect: '/signin',
-			failureFlash: true
-	   }));
+  // Set up the 'signin' routes
+  app
+    .route("/signin")
+    .get(users.renderSignin)
+    .post(
+      passport.authenticate("local", {
+        successRedirect: "/",
+        failureRedirect: "/signin",
+        failureFlash: true
+      })
+    );
 
-	// Set up the Facebook OAuth routes 
-	app.get('/oauth/facebook', passport.authenticate('facebook', {
-		failureRedirect: '/signin'
-	}));
-	app.get('/oauth/facebook/callback', passport.authenticate('facebook', {
-		failureRedirect: '/signin',
-		successRedirect: '/'
-	}));
-
-	// Set up the Twitter OAuth routes 
-	app.get('/oauth/twitter', passport.authenticate('twitter', {
-		failureRedirect: '/signin'
-	}));
-	app.get('/oauth/twitter/callback', passport.authenticate('twitter', {
-		failureRedirect: '/signin',
-		successRedirect: '/'
-	}));
-
-	// Set up the Google OAuth routes 
-	app.get('/oauth/google', passport.authenticate('google', {
-		scope: [
-			'https://www.googleapis.com/auth/userinfo.profile',
-			'https://www.googleapis.com/auth/userinfo.email'
-		],
-		failureRedirect: '/signin'
-	}));
-	app.get('/oauth/google/callback', passport.authenticate('google', {
-		failureRedirect: '/signin',
-		successRedirect: '/'
-	}));
-
-	// Set up the 'signout' route
-	app.get('/signout', users.signout);
+  // Set up the 'signout' route
+  app.get("/signout", users.signout);
 };
