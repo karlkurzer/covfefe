6a920f4fce9e1b76e716942a16110751c2cca1ac
Karl Kurzer <kurzer@fzi.de>
2018-03-04 22:18:39 +0100

    first working version with email notifications

--- app/controllers/emails.server.controller.js
+++ app/controllers/emails.server.controller.js
@@ -4,8 +4,24 @@
 // Load the module dependencies
 var mongoose = require("mongoose"),
   nodemailer = require("nodemailer"),
+  Email = require("email-templates"),
   Order = mongoose.model("Order"),
   User = mongoose.model("User");
+
+const email = new Email({
+  message: {
+    from: "covfefe@fzi.de"
+  },
+  transport: {
+    jsonTransport: true
+  },
+  views: {
+    root: "app/views",
+    options: {
+      extension: "ejs"
+    }
+  }
+});
 
 let transporter = nodemailer.createTransport({ host: "mail.fzi.de", port: 25 });
 
@@ -45,49 +61,39 @@ exports.createBalanceReceipt = function(req, res, next) {
   req.mailOptions = {
     from: "covfefe@fzi.de", // sender address
     to: "kurzer@fzi.de", // list of receivers
-    subject: "User Update", // Subject line
-    html: "<p>First Name: " + req.user.firstName + "</p>" // html body
+    subject: "Balance Update" // Subject line
   };
-  req.callback = function() {
-    return res.json(req.user);
+
+  const locals = {
+    name: req.user.firstName,
+    subject: "Balance Update",
+    balance: req.user.balance,
+    id: req.user._id
   };
-  next();
+
+  email
+    .render("covfefe", locals)
+    .then(function(res) {
+      req.mailOptions.html = res;
+      req.callback = function() {
+        return res.json(req.user);
+      };
+      next();
+    })
+    .catch(console.error);
 };
 
-// Create a new controller method that retrieves a list of articles
-exports.createOrderReceipt = function(req, res, next) {
-  // setup email data with unicode symbols
-  req.mailOptions = {
-    from: "covfefe@fzi.de", // sender address
-    to: "kurzer@fzi.de", // list of receivers
-    subject: "createOrderReceipt" // Subject line
-  };
+// // Create a new controller method that retrieves a list of articles
+// exports.createOrderReceipt = function(req, res, next) {
+//   // setup email data with unicode symbols
+//   req.mailOptions = {
+//     from: "covfefe@fzi.de", // sender address
+//     to: "kurzer@fzi.de", // list of receivers
+//     subject: "createOrderReceipt" // Subject line
+//   };
 
-  req.callback = function() {
-    return res.json(req.order);
-  };
-  next();
-};
-
-// THE FOLLOWING EMAILs need to be updated
-// Marc.Zofka@fzi.de (Marc.Zofka@fzi.de)
-// Stephan.Heuer@fzi.de (Stephan.Heuer@fzi.de)
-// Kai.Braun@fzi.de (Kai.Braun@fzi.de)
-// Marcel.Kurovski@fzi.de (Marcel.Kurovski@fzi.de)
-// Daniel.Schumacher@fzi.de (Daniel.Schumacher@fzi.de)
-// Ole.Salscheider@fzi.de (Ole.Salscheider@fzi.de)
-// Ana.Skrenkovic@fzi.de (Ana.Skrenkovic@fzi.de)
-// Marius.Gerdes@fzi.de (Marius.Gerdes@fzi.de)
-// Nick.Vlasoff@fzi.de (Nick.Vlasoff@fzi.de)
-// Kasse.SIM@fzi.de (Kasse.SIM@fzi.de)
-// Dominik.Wilke@fzi.de (Dominik.Wilke@fzi.de)
-// Cenk.Uestbes@fzi.de (Cenk.Uestbes@fzi.de)
-// Xizhe.Lian@fzi.de (Xizhe.Lian@fzi.de)
-// Sahin.Tas@fzi.de (Sahin.Tas@fzi.de)
-// Sebastian.Gottwalt@fzi.de (Sebastian.Gottwalt@fzi.de)
-// Polley.Rupert@fzi.de (Polley.Rupert@fzi.de)
-// Alide.Bornhaupt@fzi.de (Alide.Bornhaupt@fzi.de)
-// Daaboul.Karam@fzi.de (Daaboul.Karam@fzi.de)
-// Camilo.Tieck@fzi.de (Camilo.Tieck@fzi.de)
-// Kai.Hermann@fzi.de (Kai.Hermann@fzi.de)
-// Merin.Ylarfledice@fzi.de (Merin.Ylarfledice@fzi.de)
+//   req.callback = function() {
+//     return res.json(req.order);
+//   };
+//   next();
+// };
