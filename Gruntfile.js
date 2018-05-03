6f0f1ab0dbefed3c5f297f9ab8346f402b8a2202
Karl Kurzer <kurzer@fzi.de>
2018-05-03 15:40:42 +0200

    added grunt nodemon for automatic server restarts

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,6 +2,19 @@ module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
+    nodemon: {
+      dev: {
+        script: "server.js",
+        options: {
+          ext: "js,html",
+          watch: [
+            "server.js",
+            "config/**/*.js",
+            "app/**/*.js"
+          ]
+        }
+      }
+    },
     watch: {
       css: {
         files: ['public/assets/css/custom.css'],
@@ -13,9 +26,10 @@ module.exports = function(grunt) {
   });
 
   // Load the plugin that provides the "watch" task.
+  grunt.loadNpmTasks("grunt-nodemon");
   grunt.loadNpmTasks("grunt-contrib-watch");
 
   // Default task(s).
-  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('default', ['nodemon', 'watch']);
 
 };
