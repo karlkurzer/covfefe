e267b83b3473cf275e33b7fb1d79188da060e99b
Karl Kurzer <kurzer@fzi.de>
2018-05-29 14:43:00 +0200

    fixed the substring extraction

--- public/example/controllers/example.client.controller.js
+++ public/example/controllers/example.client.controller.js
@@ -11,17 +11,10 @@ angular.module('example').controller('ExampleController', ['$scope', '$http', '$
 		$http({ method: 'GET', url: 'https://api.rss2json.com/v1/api.json?rss_url=https%3A%2F%2Fwww.blogger.com%2Ffeeds%2F746298260979647434%2Fposts%2Fdefault' }).
 			then(function (response) {
 				$scope.status = response.status;
-
 				for (var i = 0; i < response.data.items.length; i++) {
 					if (response.data.items[i].title && response.data.items[i].title.startsWith("Newsticker")) {
-						$scope.data = response.data.items[i].description;
-						$scope.data = response.data.items[i].description.split("++");
-
-						for (var i = 0; i < $scope.data.length; i++) {
-							if ($scope.data[i].startsWith("+ ")) {
-								$scope.news.push("++" + $scope.data[i].trim() + " +++");
-							}
-						}
+						var newsHtml = response.data.items[i].description;
+						$scope.news = newsHtml.match(/\+\+\+.*?\+\+\+/g);
 					}
 				}
 				$scope.currentNews = $scope.news[Math.floor(Math.random() * $scope.news.length)];
