1e7ab7304abe723922429edd6fa790e4e9af37ba
Karl Kurzer <kurzer@fzi.de>
2018-10-29 17:33:44 +0100

    added a how to video

--- public/home/config/home.client.routes.js
+++ public/home/config/home.client.routes.js
@@ -8,6 +8,9 @@ angular.module('home').config(['$routeProvider',
 		when('/', {
 			templateUrl: 'home/views/home.client.view.html'
 		}).
+		when('/how_to_covfefe', {
+			templateUrl:  'home/views/how_to_covfefe.client.view.html'
+		}).
 		otherwise({
 			redirectTo: '/'
 		});
